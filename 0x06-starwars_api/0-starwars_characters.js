2e4683607f6d7645a964285bd50f4cea86fc7847
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-18 12:10:53 +0100

    Starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,32 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
-request(url, function (error, response, body) {
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
+
+request(options, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const film = JSON.parse(body);
-    console.log(film.title);
-    console.log('Characters:');
-
-    // Function to fetch and print characters recursively
-    function printCharacters (characters, index) {
-      if (index < characters.length) {
-        request(characters[index], function (charError, charResponse, charBody) {
-          if (!charError && charResponse.statusCode === 200) {
-            const character = JSON.parse(charBody);
-            console.log(character.name);
-            printCharacters(characters, index + 1);
-          } else {
-            console.error(`Error fetching character ${index + 1}: ${charError}`);
-          }
-        });
-      }
-    }
-
-    printCharacters(film.characters, 0);
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   } else {
     console.error(`Error fetching movie details: ${error}`);
   }
 });
+
+function printCharacters(characters, index) {
+  if (index < characters.length) {
+    request(characters[index], function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(JSON.parse(body).name);
+        printCharacters(characters, index + 1);
+      } else {
+        console.error(`Error fetching character details: ${error}`);
+      }
+    });
+  }
+}
